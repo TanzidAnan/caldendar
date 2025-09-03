1c00470563c3a2de2914732445c980ece5862318
TanzidAnan <tanzid.anan125@gmail.com>
2025-09-03 09:35:11 +0600

    arrow funcation

---
+++ ES8-Conser/arrow.js
@@ -0,0 +1,16 @@
+function info() {
+    return 'ki chai....?'
+}
+
+
+
+
+const result = info();
+// console.log(result)
+
+
+
+const double =(x) => x*2;
+
+const result1=double(3);
+console.log(result1)
