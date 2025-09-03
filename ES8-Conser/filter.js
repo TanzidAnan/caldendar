d4220cf168df32d0f0b0f11a39b9d79122da7c33
TanzidAnan <tanzid.anan125@gmail.com>
2025-09-04 00:19:37 +0600

    filter elements

---
+++ ES8-Conser/filter.js
@@ -0,0 +1,12 @@
+const numbers =[20,25,45,74,85,69,74,25,41];
+
+
+
+
+
+
+
+
+
+const result =numbers.filter((item) =>item >50);
+console.log(result)
--- ES8-Conser/find.js
+++ ES8-Conser/find.js
@@ -1,17 +1,17 @@
-const arr =[21,36,52,45,85,74];
+const arr = [21, 36, 52, 45, 85, 74];
 
-let find =null;
+let find = null;
 
 
-for(let item of arr){
-    if(item >50){
-        find =item;
+for (let item of arr) {
+    if (item > 50) {
+        find = item;
         break;
     }
 }
 
 // console.log(find)
+arr.filter
 
-
-const result =arr.find((item) =>item>50 && item%2 ===0);
+const result = arr.find((item) => item > 50 && item % 2 === 0);
 console.log(result)
