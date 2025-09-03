7a51c9c18ab24fa1f672fed8ac1a7ae6bd3cb264
TanzidAnan <tanzid.anan125@gmail.com>
2025-09-03 09:40:10 +0600

    arrow funcation

--- ES8-Conser/arrow.js
+++ ES8-Conser/arrow.js
@@ -13,4 +13,20 @@ const result = info();
 const double =(x) => x*2;
 
 const result1=double(3);
-console.log(result1)
+// console.log(result1)
+
+const add=(x,y) =>{
+    const total =x+y;
+    return total
+}
+
+const result2 =add(5,9);
+// console.log(result2)
+
+const add1=(x=0,y=0)=>{
+    const total =x+y;
+    return total
+}
+
+const result3=add1(5);
+console.log(result3)
