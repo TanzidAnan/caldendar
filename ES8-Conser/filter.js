e65fa8f2016afd85d875a12952d072287be777ae
TanzidAnan <tanzid.anan125@gmail.com>
2025-09-04 00:36:26 +0600

    deduce elements

--- ES8-Conser/filter.js
+++ ES8-Conser/filter.js
@@ -4,9 +4,8 @@ const numbers =[20,25,45,74,85,69,74,25,41];
 
 
 
-
-
-
+numbers.reduce
 
 const result =numbers.filter((item) =>item >50);
 console.log(result)
+console.log(!!result)
---
+++ ES8-Conser/reduce.js
@@ -0,0 +1,13 @@
+const numbers=[1,3,6,8,4,5,1,9,7,8,5];
+
+
+
+
+
+
+
+
+
+
+const sum =numbers.reduce((prev,current) =>prev +current,0);
+console.log(sum)
