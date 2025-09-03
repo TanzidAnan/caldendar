d78b7b46bafaa0e15283dd664ec574a0c9f02e63
TanzidAnan <tanzid.anan125@gmail.com>
2025-09-04 00:54:42 +0600

    template sting

--- ES8-Conser/template.js
+++ ES8-Conser/template.js
@@ -4,4 +4,21 @@ const job ='bekar';
 
 const sentence =`amar name ${name}, amar boyos ${age} ami akhon ${job}`;
 
-console.log(sentence)
+// console.log(sentence)
+
+
+const myInfo1={
+    name:'mehedy',
+    age:25,
+    address:'rajshahi',
+    sosurName:'katappa',
+    sosurBari:'B.Baria',
+    subject :['bangla','english','math'],
+    contact:{
+        phone:25454554,
+        email:'info@gmail.com'
+    }
+}
+
+const sentence1 =`amar name ${name} amar subjects gulo ${myInfo1.subject.map((sub) =>sub)}`;
+console.log(sentence1)
