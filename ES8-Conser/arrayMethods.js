0790751fcf1b97cba5e4fecce1f228443c700b18
TanzidAnan <tanzid.anan125@gmail.com>
2025-09-03 12:07:38 +0600

    for Each number

--- ES8-Conser/arrayMethods.js
+++ ES8-Conser/arrayMethods.js
@@ -5,8 +5,10 @@ const arr =[20,32,52,63,45,21];
 
 let sum =0;
 
-arr.forEach((item) =>{
-    sum =sum+item
+arr.forEach((item,index,a) =>{
+   console.log(item,index,a)
 })
 
-console.log('Total Number: ',sum)
+// console.log('Total Number: ',sum,index)
+
+// callback parameters (item,index,whole array)
