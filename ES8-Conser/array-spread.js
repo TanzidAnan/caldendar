4081d5e120c529e158964b9f844dbc7b810fafa3
TanzidAnan <tanzid.anan125@gmail.com>
2025-09-03 09:58:22 +0600

    spread operater array thake

---
+++ ES8-Conser/array-spread.js
@@ -0,0 +1,6 @@
+const arr1 =[2,5,2,4,1,5];
+const arr2 =[...arr1];
+
+arr2.push(52,84);
+console.log(arr1)
+console.log(arr2)
--- ES8-Conser/logical.js
+++ ES8-Conser/logical.js
@@ -5,4 +5,8 @@ const money =true;
 const bus ='bus ace';
 const rickkshaw ="rickshaw ace";
 
-console.log(bus|| rickkshaw)
+// console.log(bus|| rickkshaw);
+
+const x =5;
+
+console.log(x===5?'number is 5' :'not five')
