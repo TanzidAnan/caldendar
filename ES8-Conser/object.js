2b1c5217cc0f4851dff79a8b7044af93a99c0486
TanzidAnan <tanzid.anan125@gmail.com>
2025-09-03 11:36:40 +0600

    dot notation

--- ES8-Conser/object.js
+++ ES8-Conser/object.js
@@ -12,5 +12,24 @@ const myInfo={
 
 const obj2 ={...myInfo};
 obj2.married =false;
-console.log(myInfo)
-console.log(obj2)
+// console.log(myInfo)
+// console.log(obj2)
+
+
+// object access : dot notation ,bracket notation
+
+const myInfo1={
+    name:'mehedy',
+    age:25,
+    address:'rajshahi',
+    sosurName:'katappa',
+    sosurBari:'B.Baria',
+    contact:{
+        phone:25454554,
+        email:'info@gmail.com'
+    }
+}
+
+const x ='sosurName';
+console.log(myInfo1['sosurName'])
+console.log(myInfo1[x])
