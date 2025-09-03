e8429584ce2031f1b6ea03b2e9ea5e8616d78a7c
TanzidAnan <tanzid.anan125@gmail.com>
2025-09-03 12:03:01 +0600

    totle number for Each

---
+++ ES8-Conser/arrayMethods.js
@@ -0,0 +1,12 @@
+const arr =[20,32,52,63,45,21];
+// for(let item of arr){
+//     console.log(item)
+// }
+
+let sum =0;
+
+arr.forEach((item) =>{
+    sum =sum+item
+})
+
+console.log('Total Number: ',sum)
