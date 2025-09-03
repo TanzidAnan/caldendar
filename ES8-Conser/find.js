c56b0bf18eecdb397badcfeeb2008d4e84f6d4ea
TanzidAnan <tanzid.anan125@gmail.com>
2025-09-04 00:14:20 +0600

    find

---
+++ ES8-Conser/find.js
@@ -0,0 +1,17 @@
+const arr =[21,36,52,45,85,74];
+
+let find =null;
+
+
+for(let item of arr){
+    if(item >50){
+        find =item;
+        break;
+    }
+}
+
+// console.log(find)
+
+
+const result =arr.find((item) =>item>50 && item%2 ===0);
+console.log(result)
