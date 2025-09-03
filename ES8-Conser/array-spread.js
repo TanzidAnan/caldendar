ebec6c1092978db025a733061e6ed05b3ece5014
TanzidAnan <tanzid.anan125@gmail.com>
2025-09-03 10:12:34 +0600

    object

--- ES8-Conser/array-spread.js
+++ ES8-Conser/array-spread.js
@@ -1,6 +1,11 @@
-const arr1 =[2,5,2,4,1,5];
-const arr2 =[...arr1];
+const arr1 = [2, 5, 2, 4, 1, 5];
+const arr2 = [...arr1];
 
-arr2.push(52,84);
+arr2.push(52, 84);
 console.log(arr1)
 console.log(arr2)
+
+
+const nemes = ['abdul kasem', 'kuddus', 'Tanzid Anan'];
+const [_,x, y, z] = ['abdul kasem', 'kuddus', 'Tanzid Anan'];
+console.log(x,z)
---
+++ ES8-Conser/object.js
@@ -0,0 +1,16 @@
+const myInfo={
+    name:'mehedy',
+    age:25,
+    address:'rajshahi',
+    sosurName:'katappa',
+    sosurBari:'B.Baria',
+    contact:{
+        phone:25454554,
+        email:'info@gmail.com'
+    }
+}
+
+const obj2 =myInfo;
+obj2.married =false;
+console.log(myInfo)
+console.log(obj2)
