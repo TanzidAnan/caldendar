6414a972c32bdcb3da6ef72f1b64d95832d24f2c
TanzidAnan <tanzid.anan125@gmail.com>
2025-09-04 00:08:35 +0600

    map

---
+++ ES8-Conser/map.js
@@ -0,0 +1,64 @@
+// const arr =[21,36,52,45,85,74]
+
+
+
+
+
+
+
+
+
+
+
+const arr =[21,36,52,45,85,74]
+
+const result1 =arr.map((a) =>a*2);
+console.log(result1)
+
+const result =arr.forEach((a) =>{
+    // console.log(a)
+})
+
+// console.log(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
