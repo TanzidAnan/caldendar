0a3b45c0823d3f503cc37a498684bb160a493446
TanzidAnan <tanzid.anan125@gmail.com>
2025-09-04 00:45:37 +0600

    template

--- ES8-Conser/reduce.js
+++ ES8-Conser/reduce.js
@@ -1,4 +1,4 @@
-const numbers=[1,3,6,8,4,5,1,9,7,8,5];
+const numbers = [1, 3, 6, 8, 4, 5, 1, 9, 7, 8, 5];
 
 
 
@@ -9,5 +9,15 @@ const numbers=[1,3,6,8,4,5,1,9,7,8,5];
 
 
 
-const sum =numbers.reduce((prev,current) =>prev +current,0);
+const sum = numbers.reduce((prev, current) => prev + current, 0);
 console.log(sum)
+
+const number = [
+    { a: 1 },
+    { a: 2 },
+    { a: 3 },
+    { a: 4 },
+
+];
+const total =number.reduce((prev,current) => prev +current.a,0);
+console.log(total)
---
+++ ES8-Conser/template.js
@@ -0,0 +1,7 @@
+const name ='Tanzid Anan';
+const age =45;
+const job ='bekar';
+
+const sentence =`amar name ${name}, amar boyos ${age} ami akhon ${job}`;
+
+console.log(sentence)
