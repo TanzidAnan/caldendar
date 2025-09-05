3b3f076e38abe57b9a8105a357fbd3ce0abbef76
TanzidAnan <tanzid.anan125@gmail.com>
2025-09-05 20:46:46 +0600

    dot notation

--- ES8-Conser/object.js
+++ ES8-Conser/object.js
@@ -12,6 +12,17 @@ const myInfo={
 
 const obj2 ={...myInfo};
 obj2.married =false;
+
+const obj3 ={...myInfo};
+obj3.personalPhone ='01924653174';
+console.log(obj3);
+console.log(myInfo)
+
+
+
+
+
+
 // console.log(myInfo)
 // console.log(obj2)
 
@@ -31,7 +42,7 @@ const myInfo1={
 }
 
 for(let key in myInfo1){
-    console.log(myInfo1[key])
+    // console.log(myInfo1[key])
 }
 
 
